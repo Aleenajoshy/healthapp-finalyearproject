fd2c86cf1ee4f4d285825c6d84971a181e4d4e9a
Aleenajoshy <99037614+Aleenajoshy@users.noreply.github.com>
2024-10-24 22:01:13 +0530

    Add files via upload

---
+++ frontend/src/components/PatientHome.js
@@ -0,0 +1,12 @@
+// frontend/src/components/PatientHome.js
+import React from 'react';
+
+function PatientHome() {
+  return (
+    <div>
+      <h2>Welcome to Patient Home Page</h2>
+    </div>
+  );
+}
+
+export default PatientHome;
